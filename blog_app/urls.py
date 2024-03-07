25890aabdd5db3fa28a1ae9b5331deab441b2128
samruddha <samruddhakumbhar@gmail.com>
2024-03-07 11:42:07 +0530

    blog-app

---
+++ blog_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.create_blog, name='create-blog'),
+    path('search/', views.retrieve_blog, name='retrieve-blog'),
+    path('update/<int:pk>', views.update_blog, name='update-blog'),
+    path('delete/<int:pk>', views.delete_blog, name='delete-blog'),
+    path('oneblog/<int:pk>', views.get_one, name='one-blog'),
+
+]
